66237ab4364be5f358719a3aa5bf974a1f99167b
Ian McCall <ian@ianmccall.codes>
2020-06-30 16:48:43 -0500

    Add support for properly serving wasm files.

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ program
   .option('-d, --dir <path>', 'Directory')
   .parse(process.argv);
 
-app.use(express.static(program.dir));
+app.use(express.static(program.dir, {
+  setHeaders: (res, path, stat) => {
+    if (/\.wasm$/.test(path)) {
+      res.set('Content-Type', 'application/wasm');
+    }
+  }
+}));
 
 app.listen(program.port, () => console.log(`listening on port ${program.port}`));
 
